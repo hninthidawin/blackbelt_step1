197324152291ffb886e3dab47f4bdc30b0fd6f38
hninthidawin <candle.hnin@gmail.com>
2018-02-13 14:43:35 +0800

    Add files via upload

---
+++ secondAssignment.py
@@ -0,0 +1,20 @@
+from random import randrange
+import sys
+
+input_str = raw_input("Guess the number: ")
+try:
+	input_int = int(input_str)
+	if input_int >10 or input_int<1:
+		print "Only 1 to 10 is accepted"
+		sys.exit()
+except Exception as e:
+	print "only accept integer."
+	# print "Error happened in script with %s"%e
+	sys.exit()
+
+irand = randrange(1, 10)
+if irand == input_int:
+	print "Correct!"
+else:
+	print "Wrong, try again! "
+	# print irand
